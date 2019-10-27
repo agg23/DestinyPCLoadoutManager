dc56ebf7d5aa12864d5439b1e8fc1bd1befc5bf1
agg23 <adam@appcannonsoftware.com>
2019-10-27 11:05:44 -0700

    Removed Lucene and switched to Regex searching

---
+++ DestinyPCLoadoutManager/Logic/Search/SearchScorer.cs
@@ -0,0 +1,31 @@
+﻿using DestinyPCLoadoutManager.API.Models;
+using Fastenshtein;
+using System.Text.RegularExpressions;
+
+namespace DestinyPCLoadoutManager.Logic.Search
+{
+    public class SearchScorer
+    {
+        private static string namePattern = "^.*{0}.*$";
+
+        private Levenshtein levenshtein;
+        private Regex nameRegex;
+
+        public SearchScorer(string searchTerm)
+        {
+            levenshtein = new Levenshtein(searchTerm);
+            nameRegex = new Regex(string.Format(namePattern, searchTerm), RegexOptions.IgnoreCase);
+        }
+
+        public float Score(Item item)
+        {
+            float levenshteinScore = levenshtein.DistanceFrom(item.Name);
+            var name = nameRegex.Match(item.Name);
+            float nameScore = name.Success ? 1 : 0;
+
+            var score = nameScore + 1 / levenshteinScore;
+
+            return score;
+        }
+    }
+}
